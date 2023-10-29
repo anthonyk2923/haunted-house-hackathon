eae77625c059d31d8dbcb6fd411a21c6bd841175
anthonyk2923 <anthony@fprintf.com>
2023-10-29 06:38:01 +0000

    a lot of stuf

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,85 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import keysImg from "$lib/assets/keys.png";
+  import background from "$lib/assets/lm,ao.jpeg";
+  function toBlack() {
+    document.body.style.background = "black";
+    document.getElementsByClassName("hideClass").style.display = "none";
+  }
+  function toOrginal() {
+    document.body.style.background = "url(" + background + ")";
+    document.body.style.backgroundSize = "cover";
+  }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
+<div id="hideClass">
+  <br />
+  <br />
+  <h1 align="center" class="spookyText text-purple-800 text-9xl">
+    Welcome to our room!
+  </h1>
+  <hr class="border border-cyan-400" />
+  <div class="flow-root">
+    <div class="pl-4 pt-8">
+      <p class="spookyText text-red-800 text-4xl float-left" align="left">
+        In this room your goal is to find the golden key <br /> in each level,
+        there are 3 levels and each level is harder <br /> than the last. But
+        don't take too long because <br /> ghosts and monsters will try and catch
+        you.
+      </p>
+    </div>
+    <img class="float-right mb-32" src={keysImg} alt="" />
+    <div class="flex-div">
+      <h4 class="spookyText float-left pl-4 pt-4 text-purple-800 text-4xl">
+        Think you're up for the challange?
+      </h4>
+      <a
+        align="center"
+        on:mouseenter={toBlack}
+        on:mouseleave={toOrginal}
+        class="spookyText float-left pl-4 pt-4 text-4xl text-blue-400 hover:border-4 hover:border-black hover:bg-gradient-to-r from-red-800 to-stone-900 rounded-full border-black hover:text-white hover:text-opacity-75"
+        id="begin-btn"
+        href="/first"
+      >
+        lets Begin</a
+      >
+    </div>
+  </div>
+</div>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+<!-- .spookyText {
+    font-family: spookyText;
+  }
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+  #begin-button:hover {
+    transform: scale(1.65);
+    transition: transform 5s;
+  }
+</style> -->
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  :global(body) {
+    background-image: url("$lib/assets/lm,ao.jpeg");
+    background-size: cover;
+  }
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  @font-face {
+    font-family: spookyText;
+    src: url("$lib/spookyFont.otf");
+  }
+  .spookyText {
+    font-family: spookyText;
+  }
+  .toHide {
+    display: none;
+  }
+  .flex-div {
+    display: inline;
+  }
+  #begin-btn:hover {
+    border-radius: 50px;
+    padding: 10px;
+  }
+  cursor {
+    display: none;
+  }
 </style>
---
+++ src/routes/first/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const cursor = document.querySelector(".cursor");
+    cursor.style.display = "none";
+  });
+
+  const ignitecursor = (e) => {
+    const cursor = document.querySelector(".cursor");
+    cursor.style.display = "block";
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  };
+
+  document.addEventListener("mousemove", function (e) {
+    ignitecursor(e);
+  });
+
+  document.addEventListener("click", function (e) {
+    ignitecursor(e);
+  });
+</script>
+
+<a href="/first" class="text-9xl text-red-400"> RELOAD</a>
+<div class="cursor">
+  <!-- <div class="flex">
+    <h1 align="left" class="spookyText text-purple-800 text-5xl">First Room</h1>
+    <h1
+      align="right"
+      class="spookyText text-purple-800 text-5xl"
+      style="margin-left: auto;"
+    >
+      Timer:
+    </h1>
+  </div> -->
+</div>
+
+<style>
+  :bind(body) {
+    background-image: url("$lib/assets/Key1.png");
+    background-size: cover;
+  }
+  @font-face {
+    font-family: spookyText;
+    src: url("$lib/spookyFont.otf");
+  }
+  .spookyText {
+    font-family: spookyText;
+  }
+  .cursor {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 0.7);
+    position: fixed;
+    pointer-events: none;
+    z-index: 9999;
+    display: none;
+  }
+</style>
